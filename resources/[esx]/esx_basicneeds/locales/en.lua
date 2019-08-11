69ad48018b4c301fefc94f246f920fe9369b0e3a
Psycholiquid71 <jgravel1977@gmail.com>
2019-08-11 16:04:50 -0400

    esx_basicneeds

---
+++ resources/[esx]/esx_basicneeds/locales/en.lua
@@ -0,0 +1,10 @@
+Locales['en'] = {
+  ['used_bread'] = 'you have eaten ~y~1x~s~ loaf of ~b~bread~s~',
+  ['used_water'] = 'you have drank ~y~1x~s~ ~b~water~s~',
+  ['used_chickensalad'] = 'you have nomed ~y~1x~s~ ~b~Chicken_Salad_Wrap~s~',
+  ['used_gardensalad'] = 'you have eaten ~y~1x~s~ ~b~Garden_Salad~s~ like a bunny rabbit',
+  ['used_icedtea'] = 'you have chugged ~y~1x~s~ Brisk ~b~Iced_Tea~s~ and are sad its gone',
+  ['used_powerade'] = 'you have drank ~y~1x~s~ ~b~Powerade~s~ taste the salt',
+  ['energybar_consumed'] = 'you feel hyped and can now run ~y~5~w~ minutes ~o~without becoming tired~w~!',
+  ['energy_end'] = 'you feel your heart rate returning to a normal level'
+}
