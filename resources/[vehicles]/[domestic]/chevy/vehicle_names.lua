ec3fcac26514381f9321ace72da0b53b2061145f
Psycholiquid71 <jgravel1977@gmail.com>
2019-08-11 16:34:27 -0400

    Added Chevys

---
+++ resources/[vehicles]/[domestic]/chevy/vehicle_names.lua
@@ -0,0 +1,20 @@
+function AddTextEntry(key, value)
+	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
+end
+Citizen.CreateThread(function()
+    AddTextEntry('IMPALASSL', '1964 Impala SS Lowrider') -- Enter Gamename from vehicles.lua and what you want it to display.
+    AddTextEntry('C7', '2014 Chevy Corvette C7 Stingray')
+	AddTextEntry('subn', ' 2006 Chevy Suburban')
+	AddTextEntry('z2879', '1979 Chevy Camaro Z28')
+	AddTextEntry('CN69', '1969 Chevrolet Nova (Standard)')
+	AddTextEntry('CD69', '1969 Chevrolet Nova (Drag Racer)')
+	AddTextEntry('CM69', '1969 Chevrolet Nova (Monster)')
+	AddTextEntry('z112017', '2017 Chevy Camaro Z71')
+	--AddTextEntry('', '')
+	--AddTextEntry('', '')
+	--AddTextEntry('', '')
+	--AddTextEntry('', '')
+	--AddTextEntry('', '')
+	
+
+end)
