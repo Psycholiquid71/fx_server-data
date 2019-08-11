e267a06f67238ea21c1de96f8c4bd799a25a2952
Psycholiquid71 <jgravel1977@gmail.com>
2019-08-11 17:38:13 -0400

    Bentleys

---
+++ resources/[vehicles]/[imports]/bentley/vehicle_names.lua
@@ -0,0 +1,7 @@
+function AddTextEntry(key, value)
+	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
+end
+Citizen.CreateThread(function()
+    AddTextEntry('CONTGT', 'Bently Continental GT') -- Enter Gamename from vehicles.lua and what you want it to display.
+    AddTextEntry('BBENTAYGA', 'Bently Bentayga')
+end)
