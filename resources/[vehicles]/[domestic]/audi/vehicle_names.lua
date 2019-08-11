9735c259506793761ae7dec67b13425709290466
Psycholiquid71 <jgravel1977@gmail.com>
2019-08-11 16:27:33 -0400

    Added audi cars

---
+++ resources/[vehicles]/[domestic]/audi/vehicle_names.lua
@@ -0,0 +1,12 @@
+function AddTextEntry(key, value)
+	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
+end
+Citizen.CreateThread(function()
+    AddTextEntry('A8', 'Audi A8') -- Enter Gamename from vehicles.lua and what you want it to display.
+    AddTextEntry('r8ppi', 'Audi R8 PPI Razor GTR')
+    AddTextEntry('Rs4avant', 'Audi RS4')
+    AddTextEntry('s5', '2017 Audi S5')
+    AddTextEntry('RS5C', 'Audi RS5 Coupe')
+    AddTextEntry('SQ72016', 'Audi SQ7')
+    AddTextEntry('rs3', '2018 Audi RS3 Sportback')
+end)
