acbcc54be959092089edfd71fd090dd41f197049
Psycholiquid71 <jgravel1977@gmail.com>
2019-09-01 11:03:35 -0400

    Fixed Handling on cars

--- resources/[vehicles]/[domestic]/pontiac/vehicle_names.lua
+++ resources/[vehicles]/[domestic]/pontiac/vehicle_names.lua
@@ -2,6 +2,5 @@ function AddTextEntry(key, value)
 	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
 end
 Citizen.CreateThread(function()
-    AddTextEntry('Phoenix', '1977 Pontiac Trans Am') -- Enter Gamename from vehicles.lua and what you want it to display.
-    AddTextEntry('PGTO', '1965 Pontiac GT')
+   AddTextEntry('PGTO', '1965 Pontiac GT')
 end)
