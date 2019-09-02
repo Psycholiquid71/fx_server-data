6b4acdb009c52c4b2cd57b385b7f2f07ba42ed69
Psycholiquid71 <jgravel1977@gmail.com>
2019-09-01 20:36:35 -0400

    added duneloader vehicles_name

--- resources/[vehicles]/[gtav]/dloader/vehicle_names.lua
+++ resources/[vehicles]/[gtav]/dloader/vehicle_names.lua
@@ -2,6 +2,8 @@ function AddTextEntry(key, value)
 	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
 end
 Citizen.CreateThread(function()
-    AddTextEntry('gameName', 'Display_name') -- Enter Gamename from vehicles.lua and what you want it to display.
-    AddTextEntry('gameName', 'Display_name')
+    AddTextEntry('DLOADER', 'Duneloader') -- Enter Gamename from vehicles.lua and what you want it to display.
+    AddTextEntry('DLOADER3', 'Duneloader 3')
+    AddTextEntry('DLOADER4', 'Duneloader 4')
+    AddTextEntry('DLOADER5', 'Duneloader 5')
 end)
