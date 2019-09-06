b784d16203b45032defb9fb7cbd0438d4caf8dfd
Psycholiquid71 <jgravel1977@gmail.com>
2019-09-06 09:19:55 -0400

    Added bikes and cars

--- resources/[vehicles]/[domestic]/honda/vehicle_names.lua
+++ resources/[vehicles]/[domestic]/honda/vehicle_names.lua
@@ -4,5 +4,11 @@ end
 Citizen.CreateThread(function()
     AddTextEntry('nc1', '2016 Honda NSX ') -- Enter Gamename from vehicles.lua and what you want it to display.
     AddTextEntry('TypeR17', '2018 Honda Civic Type R')
-	AddTextEntry('cu2', '2018 Honda CU2')
+    AddTextEntry('cu2', '2018 Honda CU2')
+    AddTextEntry('ody18', '2018 Honda Odyssey')
+    AddTextEntry('GOLDWING', '2018 Honda Goldwing GL1800')
+    AddTextEntry('CB500X', 'Honda CB 500X')
+    --AddTextEntry('', '')
+    --AddTextEntry('', '')
+    --AddTextEntry('', '')
 end)
