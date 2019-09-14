8fcd1df0dbc7908751de31fe9f4c833af479f2b7
Psycholiquid71 <jgravel1977@gmail.com>
2019-09-14 10:24:32 -0400

    Added vehicles

--- resources/[vehicles]/[imports]/landrover/vehicle_names.lua
+++ resources/[vehicles]/[imports]/landrover/vehicle_names.lua
@@ -2,10 +2,10 @@ function AddTextEntry(key, value)
 	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
 end
 Citizen.CreateThread(function()
-    AddTextEntry('def90', '1987 Land Rover Defender') -- Enter Gamename from vehicles.lua and what you want it to display.
-    AddTextEntry('FX4', 'Land Rover Discovery')
-    AddTextEntry('landseries3', '1988 Land Rover Series 3')
-    --AddTextEntry('', '')
+    AddTextEntry('def90', '1987 Landrover Defender') -- Enter Gamename from vehicles.lua and what you want it to display.
+    AddTextEntry('FX4', 'Landrover Discovery')
+    AddTextEntry('landseries3', '1988 Landrover Series 3')
+    AddTextEntry('LRII109A', 'Landrover Series 2 109A')
     --AddTextEntry('', '')
     --AddTextEntry('', '')
     --AddTextEntry('', '')
